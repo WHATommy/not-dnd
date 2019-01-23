18c4bcf673effa70224077bce08992abc67312fe
WHATommy <tranman100598@gmail.com>
2019-01-22 20:48:09 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const app = express();
+
+mongoose.connect()
